1c90a754fe44d1e95512e477ab7d0abe794a496b
James Ricarte <uhenyou@gmail.com>
2025-12-31 01:08:59 +0800

    Add versioning support for controllers

---
+++ src/controllers/v1/users/index.ts
@@ -0,0 +1,4 @@
+export { createUser } from "./create-user.controller";
+export { getUsers } from "./get-users.controller";
+export { updateUser } from "./update-user.controller";
+export { deleteUser } from "./delete-user.controller";
--- src/routes/v1/user.routes.ts
+++ src/routes/v1/user.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createUser } from "../../controllers/users/create-user.controller";
-import { getUsers } from "../../controllers/users/get-users.controller";
-import { updateUser } from "../../controllers/users/update-user.controller";
-import { deleteUser } from "../../controllers/users/delete-user.controller";
+import {
+  createUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} from "../../controllers/v1/users";
 
 const router = express.Router();
 
